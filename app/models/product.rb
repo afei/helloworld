8f0b067f3842e898dc61b4f7e3ce7b539f2882d0
alfred@centos <afei1231@gmail.com>
2016-05-02 23:22:57 +0800

    finish task d without playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to:  0.01}
   validates :title, uniqueness: true
@@ -8,5 +9,22 @@ class Product < ActiveRecord::Base
   }
   #The validates() method will check one or more model fields against one or more condition
   #presence: true tells the validator to check that each of the named fields is present and its contents are not empty.
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+  private
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_itmes.empty?
+	return true
+      else
+	errors.add(:base, 'Line Items present')
+	return false
+      end
+    end
+
   
 end
