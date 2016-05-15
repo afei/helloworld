0bb6ed2f21ed03255192ad769fa95da9f0be5dad
afei1231 <afei1231@gmail.com>
2016-05-15 18:32:26 +0800

    add puma server

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://ruby.taobao.org'
 
+gem 'puma'
+
+gem 'actionpack-xml_parser'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
---
+++ config/puma.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env puma
+
+# rails environment
+environment 'development'
+threads 0, 16
+workers 4
+
+# application name, application path
+app_name = "html"
+application_path = "#{Rails.root}"
+#application_path = "/Users/ichr/code/workspace/depot"
+directory "#{application_path}"
+
+# puma configration
+pidfile "#{application_path}/tmp/pids/puma.pid"
+state_path "#{application_path}/tmp/sockets/puma.state"
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+bind "unix://#{application_path}/tmp/sockets/#{app_name}.sock"
+activate_control_app "unix://#{application_path}/tmp/sockets/pumactl.sock"
+
+# run as daemonize
+daemonize true
+on_restart do
+  puts 'On restart...'
+end
+
+preload_app!
+
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
