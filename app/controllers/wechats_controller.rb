79e66e19e73f5f4465df843abd91a32c8360d50a
afei1231 <afei1231@gmail.com>
2016-05-19 13:51:29 +0800

    xml file

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -5,7 +5,9 @@ class WechatsController < ApplicationController
   skip_before_action :authorize
   before_action :check_signature?
   
-  @@token = "tkn_wechat_afei1231_hotmail"
+  @token = ""
+  @appid = "wx049f75229efb0dc2"
+  @secret = ""
   
   def wechat_auth
     render text: params[:echostr]
@@ -23,7 +25,35 @@ class WechatsController < ApplicationController
   
 private
   def check_signature?
-    Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], @@token].sort.join ) == params[:signature]
+    Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], @token].sort.join ) == params[:signature]
+  end
+  
+  def get_access_token
+    if Rails.cache.read("access_token").nil?
+      uri = URI("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{@appid}&secret=#{}")
+      res = Net::HTTP.get(uri)
+      result = JSON.parse(res)
+      Rails.cache.write("access_token",result['access_token'],expires_in: 2.hours)
+    else
+      Rails.cache.read("access_token")
+    end
+  end
+  
+  def code_access_token(appid, sercet, code)
+    uri = URI("https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{appid}&secret=#{sercet}&code=#{code}&grant_type=authorization_code")
+    res = Net::HTTP.get(uri)
+    result = JSON.parse(res)
+  end
+  
+  def send_json(url,body)
+    uri = URI(url)
+    Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do   |http|
+      request= Net::HTTP::Post.new(uri,{'Content-Type'=>'application/json'})
+      request.body=body
+      puts request.body
+      response=http.request request
+      puts response.body
+    end
   end
     
 end
